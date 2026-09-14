9a0758393dfff4ebe6d185eb917448ac91c16489
sujon-258549 <sujon258549@gmail.com>
2026-09-14 11:31:06 +0600

    feat: add ScrollToTop motion component for smooth page scrolling

--- src/components/motion/scroll-to-top.tsx
+++ src/components/motion/scroll-to-top.tsx
@@ -25,7 +25,7 @@ export function ScrollToTop() {
           animate={{ opacity: 1, y: 0, scale: 1 }}
           exit={{ opacity: 0, y: 16, scale: 0.9 }}
           transition={{ duration: 0.25 }}
-          className="fixed bottom-6 right-6 z-40"
+          className="fixed bottom-20 right-4 z-40 sm:bottom-6 sm:right-6"
         >
           <Button
             size="icon"
